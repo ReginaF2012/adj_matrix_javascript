49c8dbf27339ab2031b98d649723b799c397c770
ReginaF2012 <skritschy@gmail.com>
2021-02-21 22:49:25 -0600

    addEdge method

--- adj_matrix.js
+++ adj_matrix.js
@@ -9,4 +9,21 @@ class Graph {
             }
         }
     }
+
+    addEdge(vertex1, vertex2, weight) {
+        if (vertex1 > this.size - 1 || vertex2 > this.size - 1) {
+            console.log('invalid vertex');
+        } else if (vertex1 === vertex2) {
+            console.log('same vertex');
+        } else {
+            this.matrix[vertex1][vertex2] = weight;
+            this.matrix[vertex2][vertex1] = weight;
+        }
+    }
 }
+
+let graph = new Graph(4);
+graph.addEdge(0, 1, 2);
+graph.addEdge(0, 2, 3);
+graph.addEdge(1, 3, 2);
+graph.addEdge(2, 3, 6);
