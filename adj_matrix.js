5519b7d12e8aa3eb55665cc9059de711913086c1
ReginaF2012 <skritschy@gmail.com>
2021-02-21 23:00:36 -0600

    addVertex method

--- adj_matrix.js
+++ adj_matrix.js
@@ -31,10 +31,22 @@ class Graph {
             this.matrix[vertex2][vertex1] = 0;
         } 
     }
+
+    addVertex() {
+        this.size++;
+        this.matrix.push([]);
+        for (let i = 0; i < this.size; i++) {
+            this.matrix[i][this.size - 1] = 0;
+            this.matrix[this.size - 1][i] = 0;
+        }
+    }
 }
 
-let graph = new Graph(4);
+let graph = new Graph(3);
 graph.addEdge(0, 1, 2);
 graph.addEdge(0, 2, 3);
+graph.addVertex();
 graph.addEdge(1, 3, 2);
+graph.addEdge(2, 3, 4);
+graph.removeEdge(2, 3);
 graph.addEdge(2, 3, 6);
