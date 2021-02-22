2ef05225ad79da295ea817dbe7e069deceae50ac
ReginaF2012 <skritschy@gmail.com>
2021-02-21 22:34:39 -0600

    graph constructor

---
+++ adj_matrix.js
@@ -0,0 +1,12 @@
+class Graph {
+    constructor(size = 1) {
+        this.size = size;
+        this.matrix = [];
+        for (let i = 0; i < size; i++) {
+            this.matrix.push([]);
+            for (let j = 0; j < size; j++) {
+                this.matrix[i][j] = 0;
+            }
+        }
+    }
+}
